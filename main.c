b3809201c64ed8a5af188bc6cf81c1002630fd81
Benas Budrys <benas.budrys@mif.stud.vu.lt>
2021-09-13 04:24:15 +0300

    Created short test runner

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
+// 2110541, benas.budrys@mif.stud.vu.lt, 1 užduotis, 6 variantas
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!\n");
+    int number;
+    printf("Enter number #1:\n");
+    scanf("%d", &number);
+
+    printf("Padvigubintas skaicius:\n%d\n", number*2);
+
+    printf("Enter number #2:\n");
+    scanf("%d", &number);
+
+    printf("Patrigubintas skaicius:\n%d\n", number*3);
 }
