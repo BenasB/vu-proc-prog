8833353ce407f85072aa598fd1eb051ffeef6b2e
benasb-bankera <71316641+benasb-bankera@users.noreply.github.com>
2021-10-21 17:23:27 +0300

    Added local compilaction instructions

---
+++ task-2/main-local.c
@@ -0,0 +1,84 @@
+// 2110541, benas.budrys@mif.stud.vu.lt, 2 užduotis, 10 variantas
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "./validation/validation.h"
+
+int *filter(int array[], int arraySize, int *filteredSize, int condition(int))
+{
+    // Based on: https://stackoverflow.com/a/11656585
+    int *filtered = malloc(sizeof(int) * arraySize);
+    *filteredSize = 0;
+
+    if (!filtered)
+        return NULL;
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (condition(array[i]))
+        {
+            filtered[*filteredSize] = array[i];
+            (*filteredSize)++;
+        }
+    }
+
+    return filtered;
+}
+
+int isNegative(int element)
+{
+    return element < 0;
+}
+
+int isNonNegative(int element)
+{
+    return !isNegative(element);
+}
+
+void printArray(int array[], int arraySize)
+{
+    for (int i = 0; i < arraySize; i++)
+    {
+        printf("%d ", array[i]);
+    }
+}
+
+int main()
+{
+    int arraySize;
+
+    printf("Enter array size: ");
+
+    while (getInteger(&arraySize) || arraySize <= 0)
+    {
+        printf("Bad input\n");
+    }
+
+    printf("Enter array elements: ");
+
+    int mixedArray[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        while (getInteger(&mixedArray[i]))
+        {
+            printf("Bad input\n");
+        }
+    }
+
+    int filteredSize;
+    int *negatives = filter(mixedArray, arraySize, &filteredSize, isNegative);
+    if (negatives)
+    {
+        printArray(negatives, filteredSize);
+        free(negatives);
+    }
+
+    int *nonNegatives = filter(mixedArray, arraySize, &filteredSize, isNonNegative);
+    if (nonNegatives)
+    {
+        printArray(nonNegatives, filteredSize);
+        free(nonNegatives);
+    }
+
+    printf("\n");
+}
--- task-2/readme.md
+++ task-2/readme.md
@@ -11,3 +11,7 @@
 Iš pirmos užduoties ištraukiau validavimo algoritmą, jį patobulinau ir padariau atskirą biblioteką (static library), kurią su Makefile pagalba įsirašiau į savo kompiuterį. Pagrindinius šios bibliotekos failus taip pat įdėjau prie šios užduoties ([validation direktorija](./validation)), o visą validavimo projektą galima rasti [čia](https://github.com/BenasB/c-input-validation). Sunkiausia dalis ko gero buvo atrasti būdą, kaip grąžinti array iš funkcijos. Sakyčiau, jog implementuoti patį užduoties sprendimo algoritmą truko greit - apie 1-2 val. su testavimu, tačiau daug laiko užtrukau kol išsiaiškinau, kaip sudaromos C bibliotekos (apie 10 valandų).
 
 Patobulinimui: būtų galima parašyti atskirą `validation` funkciją, kuri priimtų tik natūraliuosius skaičius (nereiktų tikrinti imant `arraySize`). Dar galbūt būtų galima sukurti `struct`, kuris laikytų array ir jo size kartu vienoje vietoje (nereiktų į funkcijas paduoti atskirai).
+
+### Kompiliavimas
+
+Neįsirašant `c-input-validation` bibliotekos į savo sistemą, galima programą sukompiliuoti tokiu būdu: `gcc main-local.c ./validation/getInteger.c -o output -std=c99`
